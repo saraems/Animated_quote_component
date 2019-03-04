a8cf2d3ca1c4df01b7ed2c70c9f130c2de95d113
Sara Waclawiec <sara.ems.94@gmail.com>
2019-03-04 11:13:54 +0100

    Updates

---
+++ precache-manifest.ed65c6907051e910b92236af16f7d248.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3357c7acf5a68f78f73",
+    "url": "/sentence_component_CL/static/css/main.a0bd3106.chunk.css"
+  },
+  {
+    "revision": "f3357c7acf5a68f78f73",
+    "url": "/sentence_component_CL/static/js/main.f3357c7a.chunk.js"
+  },
+  {
+    "revision": "2a5ecb0e3a6ee9160303",
+    "url": "/sentence_component_CL/static/js/runtime~main.2a5ecb0e.js"
+  },
+  {
+    "revision": "38c41808f217e5737277",
+    "url": "/sentence_component_CL/static/js/2.38c41808.chunk.js"
+  },
+  {
+    "revision": "b85d820c52ed87513ff82c03cb15936f",
+    "url": "/sentence_component_CL/index.html"
+  }
+];
